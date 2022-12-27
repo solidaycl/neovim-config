22f3b76b9531f9a5e21dfd0d2d4dbe6128aa16b9
csoliday <chris.soliday@renaissance.com>
2022-12-27 07:04:53 -0600

    Added vim surround

--- lua/soliday/packer.lua
+++ lua/soliday/packer.lua
@@ -32,6 +32,16 @@ return require('packer').startup(function(use)
 
 	use({'tpope/vim-fugitive'})
 
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	})
+
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		requires = {
